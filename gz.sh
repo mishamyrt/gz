29425795745f451eadb030476f97feec0956c035
mishamyrt <misha@myrt.co>
2019-01-14 22:32:24 +0300

    better rounding

--- gz.sh
+++ gz.sh
@@ -10,6 +10,7 @@ nc='\033[0m'
 zip $temp/zip $1 > /dev/null
 gzipped_size=$(wc -c < $temp/zip.zip | xargs)
 rm -r "${temp}"
-diff=$(echo "scale=4;(${gzipped_size} / ${original_size}) * 100" | bc -l | cut -c 1-5)
+diff=$(printf "%.1f\n" $(echo "scale=4;(${gzipped_size} / ${original_size}) * 100" | bc -l))
+echo $n
 echo -e "Original size: ${yellow}${original_size} bytes${nc}"
 echo -e "GZiped size: ${yellow}${gzipped_size} bytes${nc} (${diff}%)"
