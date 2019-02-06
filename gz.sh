f5aadcfbef22e0bcf483aaef74c32b3fc82f072f
mishamyrt <misha@myrt.co>
2019-02-06 17:17:55 +0300

    removed xtra code

--- gz.sh
+++ gz.sh
@@ -3,12 +3,10 @@ if [ "$#" -ne 1 ]; then
     echo "You shoud pass single file path"
     exit
 fi
-temp=$(mktemp -d)
 original_size=$(wc -c < $1 | xargs) 
 yellow='\033[0;33m'
 nc='\033[0m'
 gzipped_size=$(gzip -c $1 | wc -c | xargs)
-rm -r "${temp}"
 diff=$(printf "%.1f\n" $(echo "scale=4;(${gzipped_size} / ${original_size}) * 100" | bc -l))
 echo -e "Original size: ${yellow}${original_size} bytes${nc}"
 echo -e "GZiped size: ${yellow}${gzipped_size} bytes${nc} (${diff}%)"
